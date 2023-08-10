57172bb5d5dbe7f72f91db29d1af3575dc14be95
Amal <amol43@outlook.com>
2023-08-10 14:42:51 +0300

    write Models data

---
+++ AmazonServerClone/src/main/java/com/example/amazonserverclone/Model/MerchantStock.java
@@ -0,0 +1,24 @@
+package com.example.amazonserverclone.Model;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class MerchantStock {
+
+    @NotNull(message = "merchant stock id should not be empty")
+    private Integer id;
+
+    @NotNull(message = "product id should not be empty")
+    private Integer productId;
+
+    @NotNull(message = "merchant id should not be empty")
+    private Integer merchantId;
+
+    @NotNull(message = "stock should not be empty")
+    @Min(10)
+    private Integer stock;
+}
---
+++ AmazonServerClone/src/main/java/com/example/amazonserverclone/Model/User.java
@@ -0,0 +1,37 @@
+package com.example.amazonserverclone.Model;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.hibernate.validator.constraints.Length;
+
+@Data
+@AllArgsConstructor
+public class User {
+
+    @NotNull(message = "user id should not be empty")
+    private Integer id;
+
+    @NotEmpty(message = "username should not be empty")
+    @Length(min = 5, message = "username length should be more than 5 characters")
+    private String username;
+
+    @NotEmpty(message = "password should not be empty")
+    @Pattern(regexp = "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{6,}$")
+    private String password;
+
+    @NotEmpty(message = "email should not be empty")
+    @Pattern(regexp = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$")
+    private String email;
+
+    @NotEmpty(message = "role should not be empty")
+    @Pattern(regexp = "")
+    private String role;
+
+    @NotNull(message = "balance should not be empty")
+    @Positive(message = "balance should be positive amount")
+    private Double balance;
+}
